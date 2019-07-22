c7ccee59b9b1d3eb8a59b6c47ea42e13419cff0e
Tania Manco <tania@manco.io>
2019-07-22 16:12:54 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,60 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_arg(array)
+  array.pop(n)
+end
+
+def using_shift(array)
+  array.shif
+end
+
+def shift_with_args(array, arg)
+  array.shift(arg)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array[4].insert(element)
+end
+
+def using_uniq(array)
+  array.uniq(array)
+end
+
+def using_flatten(array(array2))
+  array.flatten(array2)
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+
+
+
+
+
+
+
+
